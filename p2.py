a82c48bfc11248275ca80a0ce619375d5f4af075
denrtf <denis.yarbar@gmail.com>
2022-10-15 17:44:04 +0300

    initiate commit

---
+++ p2.py
@@ -0,0 +1,11 @@
+class Student:
+    print("Hi!")
+    det __init__(self, height=160);
+        self.height = height
+
+
+vasya = Student()
+print(vasya.height)
+
+dima = Student(height=186)
+print(dima.height)
