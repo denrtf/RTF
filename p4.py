c4193f842616cdd6fe823b2a2cbfde9d29136a34
denrtf <denis.yarbar@gmail.com>
2022-10-22 17:39:22 +0300

    initial commit

---
+++ p4.py
@@ -0,0 +1,19 @@
+class Human:
+    height = 170
+
+
+class Student(Human):
+    pass
+
+
+class Worker(Human):
+    pass
+
+
+vasya = Student()
+dima = Worker()
+vika = Student()
+
+print(vasya.height)
+print(dima.height)
+print(vika.height)
